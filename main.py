8a8aafd1eef03481d122d1ec72d6962e95a0ecc7
Azael Bivihou <azaelbivihou1@gmail.com>
2023-07-06 13:04:57 +0100

    l'utilisation des fonctions à n reprises

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def demander_nom():
     return reponse_nom
 
 
-def demander_age():
+def demander_age(nom_personne):
     age_int = 0
     while age_int == 0:
-        age_str = input("quel est votre age ? ")
+        age_str = input(nom_personne + " quel est votre age ? ")
         try:
             age_int = int(age_str)
         except ValueError:
@@ -17,10 +17,20 @@ def demander_age():
     return age_int
 
 
-nom = demander_nom()
-age = demander_age()
-print("vous vous appelez " + nom + " vous avez " + str(age) + "ans")
-print("L'an prochaine vous aurez " + str(age+1) + "ans")
+nom1 = demander_nom()
+nom2 = demander_nom()
+nom3 = demander_nom()
+age1 = demander_age(nom1)
+age2 = demander_age(nom2)
+age3 = demander_age(nom3)
+print("\nvous vous appelez " + nom1 + " vous avez " + str(age1) + "ans")
+print("L'an prochaine vous aurez " + str(age1+1) + "ans\n")
+
+print("\nvous vous appelez " + nom2 + " vous avez " + str(age2) + "ans")
+print("L'an prochaine vous aurez " + str(age2+1) + "ans\n")
+
+print("\nvous vous appelez " + nom3 + " vous avez " + str(age3) + "ans")
+print("L'an prochaine vous aurez " + str(age3+1) + "ans\n")
 
 
 
