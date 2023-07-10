db8b1e3ce8864d1df2b69e46039d13a9789eca36
Azael Bivihou <azaelbivihou1@gmail.com>
2023-07-10 22:27:56 +0100

    amélioration du projet deplacement de la tortue en utilisant la boucle for

--- main.py
+++ main.py
@@ -104,10 +104,11 @@ import turtle
 
 t = turtle.Turtle()
 
-t.forward(30)
-t.left(90)
-t.forward(30)
-t.right(90)
+for i in range(0, 5):
+    t.forward(30)
+    t.left(90)
+    t.forward(30)
+    t.right(90)
 t.forward(30)
 
 
