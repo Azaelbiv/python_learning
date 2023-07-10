1d64e5734de6424fb26d7759b4bbea96e7e1166e
Azael Bivihou <azaelbivihou1@gmail.com>
2023-07-10 14:19:56 +0100

    Amélioration de l'exercice précédant qu'on a utilisé la boucle for

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def demander_information(nom, age):
+"""def demander_information(nom, age):
     print()
     print("\nvous vous appelez " + nom + " vous avez " + str(age) + "ans")
     print("L'an prochaine vous aurez " + str(age + 1) + "ans\n")
@@ -48,7 +48,57 @@ NOMBRE_PERSONNES = 5
 for i in range(0, NOMBRE_PERSONNES):
     nom = input("Entrer votre nom: ")
     age = demander_age(nom)
-    demander_information(nom, age)
+    demander_information(nom, age)"""
+
+
+def demander_nom():
+    reponse_nom = ""
+    while reponse_nom == "":
+        reponse_nom = input("Quel est votre nom ? ")
+
+    return reponse_nom
+
+
+def demander_age(nom_personne):
+    age_int = 0
+    while age_int == 0:
+        age_str = input(nom_personne + " Quel est votre age ? ")
+
+        try:
+            age_int = int(age_str)
+        except ValueError:
+            print("Erreur: Vous devez entrer un nombre pour l'age")
+    return age_int
+
+
+def afficher_information(nom, age):
+    print()
+    print("Vous vous appelez " + nom + " vous avez " + str(age) + "ans")
+
+    if 1 <= age < 3:
+        print("Vous etes bébé!")
+    elif age < 10:
+        print("Vous etes mineur!")
+    elif age == 17:
+        print("Vous etes presque majeur!")
+    elif 10 <= age < 18:
+        print("vous etes adolescent!")
+    elif age == 18:
+        print("Tout juste majeur: felicitations!")
+    elif age > 60:
+        print("Vous etes senior!")
+    elif age >= 18:
+        print("Vous etes majeur!")
+    else:
+        print("Information no valide!")
+    print("L'an prochain vous aurez " + str(age + 1) + "ans")
+
+
+NOMBRE_PERSONNE = int(input("Entrer le nombre de personne: "))
+for i in range(0, NOMBRE_PERSONNE):
+    nom = demander_nom()
+    age = demander_age(nom)
+    afficher_information(nom, age)
 
 
 
